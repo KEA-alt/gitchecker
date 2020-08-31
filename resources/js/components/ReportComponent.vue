e90b295a2d5f552d61db76b0d3219fd3e6d42d8e
Manon <manongm01240@gmail.com>
2020-08-31 14:38:23 +0200

    path output.json
    path

---
+++ resources/js/components/ReportComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="p-4 slider">
+            <h1 class="mt-5 d-flex justify-content-center">Rapport</h1>
+        </div>
+        <div class=" p-5  bg-white">
+  
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("Component mounted.");
+    }
+};
+
+      import messages  from '../../../public/report/projet/output.json';
+
+
+   
+</script>
