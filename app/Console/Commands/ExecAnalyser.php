8df80ab3b20f390473c68b10c0d2bf2489ebbebd
Pierrick GRANDCLEMENT-CHAFFY <pierrick.grandclement@gmail.com>
2020-09-01 15:15:08 +0200

    Sending mail after audit
    First version of the mail after the audit

--- app/Console/Commands/ExecAnalyser.php
+++ app/Console/Commands/ExecAnalyser.php
@@ -11,7 +11,7 @@ class ExecAnalyser extends Command
      *
      * @var string
      */
-    protected $signature = 'execute:analyser {link}';
+    protected $signature = 'execute:analyser {link}{rand}{reponame}';
 
     /**
      * The console command description.
@@ -38,28 +38,10 @@ class ExecAnalyser extends Command
     public function handle()
     {
         $link = $this->argument('link');
-        if(substr($link, -4) == ".git"){
-            $link = substr($link, 0, -4);
-        }
-        //recuperation des variables
-        $part = explode("/", $link);
-        $username = $part[3];
-        $reponame = $part[4];
-        $randInt = bin2hex(random_bytes(4));
-        $rand = str_shuffle(str_replace(".", "",$reponame.$username.$randInt));
-        echo 'username = '.$username.'<br>';
-        echo 'reponame = '.$reponame.'<br>';
-        echo 'link = '.$link.'<br>';
-        echo 'rand = '.$rand.'<br>';
-        echo '<br><br><br>';
+        $rand = $this->argument('rand');
+        $reponame = $this->argument('reponame');
 
-
-        // execute command
         exec('mkdir /var/www/html/gitchecker/public/report/'.$rand.' && cd /var/www/html/gitchecker/public/report/'.$rand.' && git clone '.$link.' && ../../.././vendor/bin/phpstan analyse '.$reponame.' -l 8 --error-format prettyJson > phpstan.json');     
         exec('cd /var/www/html/gitchecker/public/report/'.$rand.' && php ../../.././vendor/bin/phpcs '.$reponame.' --report=json > phpcs.json');
-        //echo exec('git clone '.$link);
-        //exec('./vendor/bin/phpstan analyse '.$username.'/'.$reponame);
-        //print output from command
-        //$this->comment( implode( PHP_EOL, $output ) );
     }
 }
--- app/Http/Controllers/AuditController.php
+++ app/Http/Controllers/AuditController.php
@@ -2,18 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use Mail;
 use Illuminate\Http\Request;
 use Artisan;
 
 class AuditController extends Controller
 {
-    public function audit(Request $request) 
-    {
+    public function audit(Request $request){
         $mail = $request->input('mail');
         $link = $request->input('link');
-        $exitCode = Artisan::call('execute:analyser', [
-            'link' => $link
-        ]);
-        return $mail.' '.$link;
+        $rand = $this->generateReportId($link);
+        $reponame = $this->getRepoName($link);
+        try{
+            $exitCode = Artisan::call('execute:analyser', [
+                'link' => $link,
+                'rand' => $rand,
+                'reponame' => $reponame
+            ]);
+            $this->sendAuditMail($mail, $link, $rand, $reponame);
+            return response()->json(['message' => 'Request completed']);
+        }catch(Exception $e){
+            return response()->json(['error' => $e]);
+        }
+    }
+
+    private static function sendAuditMail($mail, $link, $rand, $reponame){
+        $title = "Le résultat de votre analyse de sécurité sur ".$reponame;
+        $data = array( 'email' => $mail, 'subject' => $title);
+        try{
+            Mail::send('emails.audit', ['title' => $title, 'rand' => $rand], function ($m) use ($data)
+            {
+                $m->from('gitcheckerapp@gmail.com', 'Gitchecker App');
+
+                $m->subject($data['subject']);
+
+                $m->to($data['email']);
+            });
+            return response()->json(['message' => 'Request completed']);
+        }catch(Exception $e){
+            return response()->json(['error' => 'Request error']);
+        }
+    }
+
+    private static function generateReportId($link){
+        if(substr($link, -4) == ".git"){
+            $link = substr($link, 0, -4);
+        }
+        //recuperation des variables
+        $part = explode("/", $link);
+        $username = $part[3];
+        $reponame = $part[4];
+        $randInt = bin2hex(random_bytes(4));
+        $rand = str_shuffle(str_replace(".", "",$reponame.$username.$randInt));
+
+        return $rand;
+    }
+
+    private static function getRepoName($link){
+        $part = explode("/", $link);
+        $reponame = $part[4];
+
+        return $reponame;
     }
 }
