19af556bae4c4108f3cf981b462f6f8330e80803
Kevin <contact@kevin-amaral.fr>
2020-09-01 13:40:35 +0200

    Processing server phpstan phpcs

--- app/Console/Commands/ExecAnalyser.php
+++ app/Console/Commands/ExecAnalyser.php
@@ -38,9 +38,27 @@ class ExecAnalyser extends Command
     public function handle()
     {
         $link = $this->argument('link');
+        if(substr($link, -4) == ".git"){
+            $link = substr($link, 0, -4);
+        }
+        //recuperation des variables
+        $part = explode("/", $link);
+        $username = $part[3];
+        $reponame = $part[4];
+        $randInt = bin2hex(random_bytes(4));
+        $rand = str_shuffle(str_replace(".", "",$reponame.$username.$randInt));
+        echo 'username = '.$username.'<br>';
+        echo 'reponame = '.$reponame.'<br>';
+        echo 'link = '.$link.'<br>';
+        echo 'rand = '.$rand.'<br>';
+        echo '<br><br><br>';
+
+
         // execute command
-        exec("bash exakat/exakat.sh ".$link, $output);
-        // print output from command
-        $this->comment( implode( PHP_EOL, $output ) );
+        exec('mkdir /var/www/html/gitchecker/public/report/'.$rand.' && cd /var/www/html/gitchecker/public/report/'.$rand.' && git clone '.$link.' && ../../.././vendor/bin/phpstan analyse '.$reponame.' -l 8 --e$        exec('cd /var/www/html/gitchecker/public/report/'.$rand.' && php ../../.././vendor/bin/phpcs '.$reponame.' --report=json > phpcs.json');
+        //echo exec('git clone '.$link);
+        //exec('./vendor/bin/phpstan analyse '.$username.'/'.$reponame);
+        //print output from command
+        //$this->comment( implode( PHP_EOL, $output ) );
     }
 }
