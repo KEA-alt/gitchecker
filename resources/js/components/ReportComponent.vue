b6733160a8bb69eb0e3000212cfda5a936c222f7
Kevin <contact@kevin-amaral.fr>
2020-09-01 09:08:44 +0200

    Processing json files in vuejs
    dynamic display of json file data in list view

--- resources/js/components/ReportComponent.vue
+++ resources/js/components/ReportComponent.vue
@@ -3,21 +3,39 @@
         <div class="p-4 slider">
             <h1 class="mt-5 d-flex justify-content-center">Rapport</h1>
         </div>
+        
         <div class=" p-5  bg-white">
-  
+            <ul id="v-for-object" class="demo">
+                <li v-for="(message, name) in content.files" :key="message">
+                    Il y a {{ message.errors }} erreurs dans le fichier : <br>{{ name }} <br><br> 
+                    <div>
+                        <li v-for="(message, name) in message.messages" :key="message">Erreur numéro : {{ name }}<br>Message : {{message.message}}<br>Ligne : {{message.line}}<br></li>
+                    </div> 
+                    
+                    <br><br><br><br>
+                </li>
+            </ul>
+
         </div>
     </div>
 </template>
 
 <script>
+import contentJson from '../../../public/report/projet/output.json'
 export default {
     mounted() {
         console.log("Component mounted.");
-    }
-};
+    },
 
-      import messages  from '../../../public/report/projet/output.json';
-
-
-   
+    data() {
+            return {
+  
+                content: []
+            }
+        },
+        created() {
+            this.content = contentJson
+            console.log(this.content);
+            }
+}; 
 </script>
