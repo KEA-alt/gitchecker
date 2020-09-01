b0ddbbea0befcfe234998961047f7abd4a6f0803
Kevin <contact@kevin-amaral.fr>
2020-09-01 19:31:18 +0200

    BootStrapVue

--- resources/js/components/ReportComponent.vue
+++ resources/js/components/ReportComponent.vue
@@ -4,7 +4,6 @@
             <h1 class="mt-5 d-flex justify-content-center">Rapport</h1>
         </div>
         <div class=" p-5  bg-white">
-            <b-alert variant="success" show>Success Alert</b-alert>
             <div id="accordion" class="container">
                 <nav>
                     <div class="nav nav-tabs" id="nav-tab" role="tablist">
@@ -17,67 +16,63 @@
                 <div class="tab-content" id="nav-tabContent">
                     <div class="tab-pane fade show active" id="nav-phpstan" role="tabpanel"
                         aria-labelledby="nav-phpstan-tab">
-                        <div class="card" v-for="(message, name, index) in contentPhpStan.files" :key="message.message">
-                            <div class="card-header text-center" v-bind:id="'heading'+index">
-                                <h5 class="mb-0">
-                                    <button class="btn btn-link collapsed" data-toggle="collapse"
-                                        v-bind:data-target="'#collapse'+index" aria-expanded="false"
-                                        v-bind:aria-controls="'collapse'+index">
+                        <div role="tablist" v-for="(message, name, index) in contentPhpStan.files" :key="name.value">
+                            <b-card no-body class="mb-1">
+                                <b-card-header header-tag="header" class="p-0" role="tab">
+                                    <b-button block v-b-toggle="'accordion-'+index" variant="light">
                                         <p>Il y a {{ message.errors }} erreurs dans le fichier : </p>
-                                        <p><strong>{{ name }}</strong></p>
-                                    </button>
-                                </h5>
-                            </div>
-                            <div v-bind:id="'collapse'+index" class="collapse" v-bind:aria-labelledby="'heading'+index"
-                                data-parent="#accordion" v-for="(message, name) in message.messages"
-                                :key="message.message">
-                                <template v-if="message.ignorable">
-                                    <div class="card-body alert-warning">
-                                        <p><strong>Erreur numéro : </strong>{{ name }}</p>
-                                        <p><strong>Message : </strong>{{message.message}}</p>
-                                        <p><strong>Ligne : </strong>{{message.line}}</p>
-                                    </div>
-                                </template>
-                                <template v-else>
-                                    <div class="card-body alert-danger">
-                                        <p><strong>Erreur numéro : </strong>{{ name }}</p>
-                                        <p><strong>Message : </strong>{{message.message}}</p>
-                                        <p><strong>Ligne : </strong>{{message.line}}</p>
-                                    </div>
-                                </template>
-                            </div>
+                                        <p>{{ name }}</p></b-button>
+                                </b-card-header>
+                                <b-collapse v-bind:id="'accordion-'+index" accordion="my-accordion" role="tabpanel" v-for="(message, name) in message.messages" :key="name.value">
+                                    <template v-if="message.ignorable">
+                                        <b-card-body>
+                                            <h4><b-badge style="vertical-align: top;" variant="warning">Attention</b-badge></h4>
+                                            <b-card-text><p><strong>Erreur numéro :</strong> {{ name }}</p></b-card-text>
+                                            <b-card-text><p><strong>Message : </strong>{{message.message}}</p></b-card-text>
+                                            <b-card-text><p><strong>Ligne : </strong>{{message.line}}</p></b-card-text>
+                                        </b-card-body>
+                                    </template>
+                                    <template v-else>
+                                        <b-card-body>
+                                            <h4><b-badge style="vertical-align: top;" variant="danger">Critique</b-badge></h4>
+                                            <b-card-text><strong>Erreur numéro : </strong>{{ name }}</b-card-text>
+                                            <b-card-text><p><strong>Message : </strong>{{message.message}}</p></b-card-text>
+                                            <b-card-text><p><strong>Ligne : </strong>{{message.line}}</p></b-card-text>
+                                        </b-card-body>
+                                    </template>
+                                    
+                                </b-collapse>
+                            </b-card>
                         </div>
                     </div>
                     <div class="tab-pane fade" id="nav-phpcs" role="tabpanel" aria-labelledby="nav-phpcs-tab">
-                        <div class="card" v-for="(message, name, index) in contentPhpCS.files" :key="name.value">
-                            <div class="card-header text-center" v-bind:id="'heading'+index">
-                                <h5 class="mb-0">
-                                    <button class="btn btn-link collapsed" data-toggle="collapse"
-                                        v-bind:data-target="'#collapse'+index" aria-expanded="false"
-                                        v-bind:aria-controls="'collapse'+index">
+                        <div role="tablist" v-for="(message, name, index) in contentPhpCS.files" :key="name.value">
+                            <b-card no-body class="mb-1">
+                                <b-card-header header-tag="header" class="p-0" role="tab">
+                                    <b-button block v-b-toggle="'accordion-'+index" variant="light">
                                         <p>Il y a {{ message.errors }} erreurs dans le fichier : </p>
-                                        <p><strong>{{ name }}</strong></p>
-                                    </button>
-                                </h5>
-                            </div>
-                            <div v-bind:id="'collapse'+index" class="collapse" v-bind:aria-labelledby="'heading'+index"
-                                data-parent="#accordion" v-for="(message, name) in message.messages"
-                                :key="name.value">
-                                <template v-if="message.ignorable">
-                                    <div class="card-body alert-danger">
-                                        <p><strong>Erreur numéro : </strong>{{ name }}</p>
-                                        <p><strong>Message : </strong>{{message.message}}</p>
-                                        <p><strong>Ligne : </strong>{{message.line}}</p>
-                                    </div>
-                                </template>
-                                <template v-else>
-                                    <div class="card-body alert-warning">
-                                        <p><strong>Erreur numéro : </strong>{{ name }}</p>
-                                        <p><strong>Message : </strong>{{message.message}}</p>
-                                        <p><strong>Ligne : </strong>{{message.line}}</p>
-                                    </div>
-                                </template>
-                            </div>
+                                        <p>{{ name }}</p></b-button>
+                                </b-card-header>
+                                <b-collapse v-bind:id="'accordion-'+index" accordion="my-accordion" role="tabpanel" v-for="(message, name) in message.messages" :key="name.value">
+                                    <template v-if="message.ignorable">
+                                        <b-card-body>
+                                            <h4><b-badge style="vertical-align: top;" variant="danger">Critique</b-badge></h4>
+                                            <b-card-text><p><strong>Erreur numéro :</strong> {{ name }}</p></b-card-text>
+                                            <b-card-text><p><strong>Message : </strong>{{message.message}}</p></b-card-text>
+                                            <b-card-text><p><strong>Ligne : </strong>{{message.line}}</p></b-card-text>
+                                        </b-card-body>
+                                    </template>
+                                    <template v-else>
+                                        <b-card-body>
+                                            <h4><b-badge style="vertical-align: top;" variant="warning">Attention</b-badge></h4>
+                                            <b-card-text><strong>Erreur numéro : </strong>{{ name }}</b-card-text>
+                                            <b-card-text><p><strong>Message : </strong>{{message.message}}</p></b-card-text>
+                                            <b-card-text><p><strong>Ligne : </strong>{{message.line}}</p></b-card-text>
+                                        </b-card-body>
+                                    </template>
+                                    
+                                </b-collapse>
+                            </b-card>
                         </div>
                     </div>
                 </div>
