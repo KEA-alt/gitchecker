aa1d147a4de0ad07c1aa38f2be2f546f11f44829
Pierrick GRANDCLEMENT-CHAFFY <pierrick.grandclement@gmail.com>
2020-09-01 14:18:11 +0200

    Analyser menu
    The analyser menu as been added to allow to user to navigate between all analysers

--- resources/js/components/ReportComponent.vue
+++ resources/js/components/ReportComponent.vue
@@ -5,64 +5,49 @@
         </div>
         <div class=" p-5  bg-white">
             <div id="accordion" class="container">
-                <div class="card" v-for="(message, name, index) in contentPhpStan.files" :key="message.message">
-                    <div class="card-header text-center" v-bind:id="'heading'+index">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link collapsed" data-toggle="collapse"
-                                v-bind:data-target="'#collapse'+index" aria-expanded="false"
-                                v-bind:aria-controls="'collapse'+index">
-                                <p>Il y a {{ message.errors }} erreurs dans le fichier : </p>
-                                <p><strong>{{ name }}</strong></p>
-                            </button>
-                        </h5>
+                <nav>
+                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                        <a class="nav-link active" id="nav-phpstan-tab" data-toggle="tab" href="#nav-phpstan" role="tab"
+                            aria-controls="nav-phpstan" aria-selected="true">PhpStan</a>
+                        <a class="nav-link" id="nav-phpcs-tab" data-toggle="tab" href="#nav-phpcs" role="tab"
+                            aria-controls="nav-phpcs" aria-selected="false">PhpCodeSniffer</a>
                     </div>
-                    <div v-bind:id="'collapse'+index" class="collapse" v-bind:aria-labelledby="'heading'+index"
-                        data-parent="#accordion" v-for="(message, name) in message.messages" :key="message.message">
-                        <template v-if="message.ignorable">
-                            <div class="card-body alert-warning">
-                                <p><strong>Erreur numéro : </strong>{{ name }}</p>
-                                <p><strong>Message : </strong>{{message.message}}</p>
-                                <p><strong>Ligne : </strong>{{message.line}}</p>
+                </nav>
+                <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane fade show active" id="nav-phpstan" role="tabpanel"
+                        aria-labelledby="nav-phpstan-tab">
+                        <div class="card" v-for="(message, name, index) in contentPhpStan.files" :key="message.message">
+                            <div class="card-header text-center" v-bind:id="'heading'+index">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link collapsed" data-toggle="collapse"
+                                        v-bind:data-target="'#collapse'+index" aria-expanded="false"
+                                        v-bind:aria-controls="'collapse'+index">
+                                        <p>Il y a {{ message.errors }} erreurs dans le fichier : </p>
+                                        <p><strong>{{ name }}</strong></p>
+                                    </button>
+                                </h5>
                             </div>
-                        </template>
-                        <template v-else>
-                            <div class="card-body alert-danger">
-                                Erreur numéro : {{ name }}<br>Message : {{message.message}}<br>Ligne :
-                                {{message.line}}<br>
+                            <div v-bind:id="'collapse'+index" class="collapse" v-bind:aria-labelledby="'heading'+index"
+                                data-parent="#accordion" v-for="(message, name) in message.messages"
+                                :key="message.message">
+                                <template v-if="message.ignorable">
+                                    <div class="card-body alert-warning">
+                                        <p><strong>Erreur numéro : </strong>{{ name }}</p>
+                                        <p><strong>Message : </strong>{{message.message}}</p>
+                                        <p><strong>Ligne : </strong>{{message.line}}</p>
+                                    </div>
+                                </template>
+                                <template v-else>
+                                    <div class="card-body alert-danger">
+                                        <p><strong>Erreur numéro : </strong>{{ name }}</p>
+                                        <p><strong>Message : </strong>{{message.message}}</p>
+                                        <p><strong>Ligne : </strong>{{message.line}}</p>
+                                    </div>
+                                </template>
                             </div>
-                        </template>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
-        <div class=" p-5  bg-white">
-            <div id="accordion" class="container">
-                <div class="card" v-for="(message, name, index) in contentPhpStan.files" :key="message.message">
-                    <div class="card-header text-center" v-bind:id="'heading'+index">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link collapsed" data-toggle="collapse"
-                                v-bind:data-target="'#collapse'+index" aria-expanded="false"
-                                v-bind:aria-controls="'collapse'+index">
-                                <p>Il y a {{ message.errors }} erreurs dans le fichier : </p>
-                                <p><strong>{{ name }}</strong></p>
-                            </button>
-                        </h5>
-                    </div>
-                    <div v-bind:id="'collapse'+index" class="collapse" v-bind:aria-labelledby="'heading'+index"
-                        data-parent="#accordion" v-for="(message, name) in message.messages" :key="message.message">
-                        <template v-if="message.ignorable">
-                            <div class="card-body alert-warning">
-                                <p><strong>Erreur numéro : </strong>{{ name }}</p>
-                                <p><strong>Message : </strong>{{message.message}}</p>
-                                <p><strong>Ligne : </strong>{{message.line}}</p>
-                            </div>
-                        </template>
-                        <template v-else>
-                            <div class="card-body alert-danger">
-                                Erreur numéro : {{ name }}<br>Message : {{message.message}}<br>Ligne :
-                                {{message.line}}<br>
-                            </div>
-                        </template>
+                    <div class="tab-pane fade" id="nav-phpcs" role="tabpanel" aria-labelledby="nav-phpcs-tab">PhpCS
                     </div>
                 </div>
             </div>
@@ -75,6 +60,7 @@
         data() {
             return {
                 contentPhpStan: [],
+                contentPhpCS: [],
                 reportId: this.$route.params.reportId
             }
         },
@@ -85,7 +71,7 @@
             }).catch(err => console.error(err));
 
             axios.get('/report/' + this.reportId + '/phpcs.json').then(response => {
-                this.contentPhpStan = response["data"];
+                this.contentPhpCS = response["data"];
             }).catch(err => console.error(err));
         },
 
