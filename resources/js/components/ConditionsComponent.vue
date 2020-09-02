e99916aed3b00729ee4770dfa4e4038d22ec68b3
Manon <manongm01240@gmail.com>
2020-09-02 15:48:38 +0200

    path /politique / condition page
    add button footer  template 
    path for new pages
    content of pages boostrap vue

---
+++ resources/js/components/ConditionsComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="p-4 slider">
+        <h1 class="mt-5 d-flex justify-content-center">Mentions Légales</h1>
+    </div>
+    <div class="p-5 bg-white">
+    <h2>Propriétaire :</h2>
+        <p>Nom</p>
+        <p>SIRET</p>
+        <p>Adresse</p>
+        <h2>Hébergement</h2>
+        <p>Adresse web</p>
+        <p>Adresse postale</p>
+        <p>Téléphone</p>
+        <p>Mail</p>
+    </diV>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  },
+};
+</script>
--- resources/js/components/DonateComponent.vue
+++ resources/js/components/DonateComponent.vue
@@ -1,36 +1,34 @@
 <template>
-    <div>
-        <div class="p-4 slider">
-            <h1 class="mt-5 d-flex justify-content-center">Donation</h1>
-        </div>
-        <div class=" p-5  bg-white">
-            <h2 class="d-flex justify-content-center">
-                Pour notre fonctionnement quotidien
-            </h2>
-            <p class="d-flex justify-content-center">
-                Vous avez aimé notre outil et vous avez envie de nous aider mais
-                vous ne savez pas comment, faites un don pour soutenir notre
-                équipe !
-            </p>
-            <div class="d-flex justify-content-center">
-                <a
-                    class="btn btn-dark btn-lg p-3 m-3"
-                    href="https://www.leetchi.com/fr/c/lvjY886l"
-                    >Nous soutenir</a
-                >
-            </div>
-            <div class="d-flex justify-content-center mt-2 p-3">
-                <img class="" width="150" height="150" src="img/donate.png" />
-            </div>
-            <h3 class="d-flex justify-content-center">Merci</h3>
-        </div>
+  <div>
+    <div class="p-4 slider">
+      <h1 class="mt-5 d-flex justify-content-center">Donation</h1>
     </div>
+    <div class="p-5 bg-white">
+      <h2 class="d-flex justify-content-center">Pour notre fonctionnement quotidien</h2>
+      <p class="d-flex justify-content-center">
+        Vous avez aimé notre outil et vous avez envie de nous aider mais
+        vous ne savez pas comment, faites un don pour soutenir notre
+        équipe !
+      </p>
+      <div class="d-flex justify-content-center">
+        <b-button
+          class="px-5"
+          variant="dark"
+          href="https://www.leetchi.com/fr/c/lvjY886l"
+        >Nous soutenir</b-button>
+      </div>
+      <div class="d-flex justify-content-center mt-2 p-3">
+        <img class width="150" height="150" src="img/donate.png" />
+      </div>
+      <h3 class="d-flex justify-content-center">Merci</h3>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    mounted() {
-        console.log("Component mounted.");
-    }
+  mounted() {
+    console.log("Component mounted.");
+  },
 };
 </script>
