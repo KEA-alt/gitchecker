7cdeda45438ee304dae5c71495baf666d258f31f
Pierrick GRANDCLEMENT-CHAFFY <pierrick.grandclement@gmail.com>
2020-09-02 13:33:08 +0200

    Laravel secure input
    Check if valid mail and Github url

--- app/Http/Controllers/AuditController.php
+++ app/Http/Controllers/AuditController.php
@@ -12,20 +12,28 @@ class AuditController extends Controller
     public function audit(Request $request){
         $mail = $request->input('mail');
         $link = $request->input('link');
-        if(substr($link, -4) == ".git"){
-            $link = substr($link, 0, -4);
-        }
-        $rand = $this->generateReportId($link);
-        $reponame = $this->getRepoName($link);
-        try{
-            $this->dispatch(new ExecuteAudit($link,$rand,$reponame));
-            $view = 'emails.audit';
-            $title = "Le résultat de votre analyse de sécurité sur ".$reponame;
-            $data = array('fromMail' => 'gitcheckerapp@gmail.com','fromName' => 'Gitchecker App', 'subject' => $title, 'toMail' => $mail);
-            $this->dispatch(new SendMail($view,$title,$rand,$data));
-            return response()->json(['message' => 'Request completed']);
-        }catch(Exception $e){
-            return response()->json(['error' => $e]);
+        if(filter_var($mail, FILTER_VALIDATE_EMAIL)){
+            if($this->checkGitLink($link)){
+                if(substr($link, -4) == ".git"){
+                    $link = substr($link, 0, -4);
+                }
+                $rand = $this->generateReportId($link);
+                $reponame = $this->getRepoName($link);
+                try{
+                    $this->dispatch(new ExecuteAudit($link,$rand,$reponame));
+                    $view = 'emails.audit';
+                    $title = "Le résultat de votre analyse de sécurité sur ".$reponame;
+                    $data = array('fromMail' => 'gitcheckerapp@gmail.com','fromName' => 'Gitchecker App', 'subject' => $title, 'toMail' => $mail);
+                    $this->dispatch(new SendMail($view,$title,$rand,$data));
+                    return response()->json(['message' => 'Request completed']);
+                }catch(Exception $e){
+                    return response()->json(['error' => $e]);
+                }
+            }else{
+                return response()->json(['error' => 'Invalid Github URL']);
+            }
+        }else{
+            return response()->json(['error' => 'Invalid mail address']);
         }
     }
 
@@ -45,5 +53,17 @@ class AuditController extends Controller
         $reponame = $part[4];
 
         return $reponame;
+    }
+
+    private static function checkGitLink($url){
+        if(strpos($url, 'github.com') != false){
+            if(filter_var($url, FILTER_VALIDATE_URL)) {
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
     }
 }
