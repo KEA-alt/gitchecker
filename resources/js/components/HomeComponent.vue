e32b9e24ae6c350419cfea4be5280fa12088d68d
Kevin <contact@kevin-amaral.fr>
2020-09-02 13:28:41 +0200

    Style Home component.vue

--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -8,7 +8,7 @@
                     votre Git en quelques cliques
                 </h2>
                 <div class="d-flex justify-content-center m-5">
-                    <b-button class="px-5" variant="outline-primary">GO</b-button>
+                    <b-button class="px-5" variant="dark">GO</b-button>
                 </div>
             </div>
         </div>
@@ -18,9 +18,9 @@
                     <b-card-img src="img/gitcheckerHome.png" style="width: 30%; margin: 2em 0;" img-alt="Image" img-top>
                     </b-card-img>
                     <b-card-text>
-                        This is a wider card with supporting text below as a
-                        natural lead-in to additional content. This content
-                        is a little bit longer.
+                        Un outil simple qui permet de connaître toutes les
+                        failles de son code en quelques cliques pour plus de
+                        fiabilité !
                     </b-card-text>
                 </b-card>
 
@@ -28,9 +28,8 @@
                     <b-card-img src="img/gitcheckerHome2.png" style="width: 30%; margin: 2em 0;" img-alt="Image"
                         img-top></b-card-img>
                     <b-card-text>
-                        This is a wider card with supporting text below as a
-                        natural lead-in to additional content. This content
-                        is a little bit longer.
+                        Un résultat d'audit de sécurité rapide, complet, épuré
+                        et facile à comprendre à la porté de tous
                     </b-card-text>
                 </b-card>
 
@@ -38,32 +37,34 @@
                     <b-card-img src="img/gitcheckerHome3.png" style="width: 30%; margin: 2em 0;" img-alt="Image"
                         img-top></b-card-img>
                     <b-card-text>
-                        This is a wider card with supporting text below as a
-                        natural lead-in to additional content. This content
-                        is a little bit longer.
+                        Un fois fini n'hésitez pas à nous faire un retour de
+                        votre experience avec GitChecker pour une amélioration
+                        en continue. Si vous avez aimé vous pouvez partager nos
+                        réseaux sociaux ou laisser un don sur la cagnotte !
                     </b-card-text>
                 </b-card>
             </div>
         </div>
 
 
-        <div class="row align-items-center bg-white">
-            <b-card no-body class="overflow-hidden" style="max-width: 80%; margin: 2em auto;">
+        <div class="row align-items-center">
+            <b-card no-body class="overflow-hiddenp-3" border-variant="white">
                 <b-row no-gutters>
-                    <b-col md="6">
-                        <b-card-img src="img/ordi.png" alt="Image" class="rounded-0">
-                        </b-card-img>
+                    <b-col md="6" class="d-flex justify-content-center">
+                        <b-card-img src="img/ordi.png" alt="Image" class="rounded-0 gitImg"></b-card-img>
                     </b-col>
                     <b-col md="6">
-                        <b-card-body class="p-5" title="TITRE BLOC">
+                        <b-card-body class="p-5" title="Comment ça marche ?">
                             <b-card-text class="py-5">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Mauris pretium vehicula orci. Integer at porta risus, ut
-                                eleifend ipsum. Quisque molestie ligula sapien, sed mollis
-                                lectus malesuada eget. Pellentesque habitant morbi tristique
-                                senectus et netus et malesuada fames ac turpis egestas.
-                                Phasellus mauris diam, sodales id urna ac, condimentum
-                                convallis purus.
+                                Copiez le lien de votre Git à analyser.
+                                <br />Allez dans la page Audit de sécurité, coller
+                                votre Url plus cliquez sur le bouton
+                                Analyser.
+                                <br />Le rapport complet vous sera envoyé par mail dans quelques
+                                secondes.
+                                <br />Corrigez votre code. Vous avez terminé !
+                                <br />Maintenant votre code et sans fautes, propore et
+                                sécurisé.
                             </b-card-text>
                         </b-card-body>
                     </b-col>
