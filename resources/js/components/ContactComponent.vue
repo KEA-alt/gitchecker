9778442d236a656fe6c00e6a57dea8fcbedc0ba3
Kevin <contact@kevin-amaral.fr>
2020-09-03 10:48:48 +0200

    ContactVue.js fix

--- resources/js/components/ContactComponent.vue
+++ resources/js/components/ContactComponent.vue
@@ -24,30 +24,51 @@
         </div>
 
         <div class=" bg-white pb-5">
-            <div class="container">
-                <form @submit.prevent="submitContact">
+
+            <div class="my-5 px-5">
+                <b-form @submit.prevent="submitContact">
                     <div class="form-row">
                         <div class="form-group col-md-6">
-                            <input type="text" class="form-control" placeholder="Prénom" id="firstname" name="firstname"
-                                v-model="contactFirstname">
+                            <b-form-group id="fieldset-1" description="Communiquez-nous votre nom."
+                                label="Entrez votre nom" label-for="idcontactLastname"
+                                :invalid-feedback="lastnameInvalidFeedback" :valid-feedback="lastnameValidFeedback"
+                                :state="lastnameState">
+                                <b-form-input id="idcontactLastname" v-model="contactLastname" :state="lastnameState"
+                                    trim></b-form-input>
+                            </b-form-group>
                         </div>
                         <div class="form-group col-md-6">
-                            <input type="text" class="form-control" placeholder="Nom" id="lastname" name="lastname"
-                                v-model="contactLastname">
+                            <b-form-group id="fieldset-1" description="Communiquez-nous votre prénom."
+                                label="Entrez votre prénom" label-for="idcontactFirstname"
+                                :invalid-feedback="firstnameInvalidFeedback" :valid-feedback="firstnameValidFeedback"
+                                :state="firstnameState">
+                                <b-form-input id="idcontactFirstname" v-model="contactFirstname" :state="firstnameState"
+                                    trim></b-form-input>
+                            </b-form-group>
                         </div>
                     </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-12">
-                            <input type="email" class="form-control" placeholder="Email" id="email" name="email"
-                                v-model="contactEmail">
-                        </div>
-                        <textarea class="form-control" placeholder="Message" id="content" rows="3" name="content"
-                            v-model="contactMessage"></textarea>
-                        <div class="m-2 buttonContainer mx-auto">
-                            <button class="btn btn-info text-white" type="submit">Envoyer</button>
-                        </div>
-                    </div>
-                </form>
+
+                    <b-form-input type="email" v-model="contactEmail" :state="mailState" placeholder="E-mail" trim>
+
+                            <b-form-input id="idcontactFirstname" v-model="contactFirstname" :state="mailState"
+                                trim></b-form-input>
+                    </b-form-input>
+
+                    <b-form-group id="fieldset-1" description="dit nous tout, on s'occupe du reste"
+                        label="Votre message : " label-for="textarea" :invalid-feedback="textAreaInvalidFeedback"
+                        :valid-feedback="textAreaValidFeedback" :state="textAreaState">
+
+                        <b-form-textarea id="textarea" class="my-3" v-model="contactMessage" rows="3" max-rows="6" :state="textAreaState">
+                        </b-form-textarea>
+
+                    </b-form-group>
+
+                    <b-button type="submit" :disabled="btnState" variant="dark">Envoyer</b-button>
+
+                    <h3 id="pMsg" class="py-5 justify-content-center text-center">Requête envoyée</h3>
+
+
+                </b-form>
             </div>
         </div>
     </div>
@@ -55,11 +76,119 @@
 
 <script>
     export default {
+        computed: {
+            mailState() {
+                if (this.contactEmail) {
+                    var re =
+                        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    if (re.test(this.contactEmail)) {
+                        this.btnState1 = true;
+                        return true;
+                    } else {
+                        this.btnState1 = false;
+                        return false;
+                    }
+                }
+            },
+            lastnameState() {
+                if (this.contactLastname) {
+                    if(this.contactLastname.length >= 4){
+                        this.btnState2 = true;
+                        return true;
+                    }else{
+                        this.btnState2 = false;
+                        return false;
+                    }
+                }
+            },
+            lastnameInvalidFeedback() {
+                if (this.contactLastname) {
+                    if (this.contactLastname.length > 4) {
+                        return ''
+                    } else if (this.contactLastname.length > 0) {
+                        return 'Entrez au moins 4 caractères'
+                    } else {
+                        return 'Veuillez saisir quelque chose'
+                    }
+                }
+            },
+            lastnameValidFeedback() {
+                if (this.contactLastname) {
+                    return this.contactLastname === true ? 'Merci' : ''
+                }
+            },
+            firstnameState() {
+                if (this.contactFirstname) {
+                    if(this.contactFirstname.length >= 4){
+                        this.btnState3 = true;
+                        return true;
+                    }else{
+                        this.btnState3 = false;
+                        return false;
+                    }
+                }
+            },
+            firstnameInvalidFeedback() {
+                if (this.contactFirstname) {
+                    if (this.contactFirstname.length > 4) {
+                        return ''
+                    } else if (this.contactFirstname.length > 0) {
+                        return 'Entrez au moins 4 caractères'
+                    } else {
+                        return 'Veuillez saisir quelque chose'
+                    }
+                }
+            },
+            firstnameValidFeedback() {
+                if (this.contactFirstname) {
+                    return this.contactFirstname === true ? 'Merci' : ''
+                }
+            },
+
+            textAreaState() {
+                if (this.contactMessage) {
+                    if(this.contactMessage.length >= 20 ? true : false){
+                        this.btnState4 = true;
+                        return true;
+                    }else{
+                        this.btnState4 = false;
+                        return false;
+                    }
+                }
+            },
+            textAreaInvalidFeedback() {
+                if (this.contactMessage) {
+                    if (this.contactMessage.length > 20) {
+                        return ''
+                    } else if (this.contactMessage.length > 0) {
+                        return 'Entrez au moins 20 caractères'
+                    } else {
+                        return 'Veuillez saisir quelque chose'
+                    }
+                }
+            },
+            textAreaValidFeedback() {
+                if (this.contactMessage) {
+                    return this.contactMessage === true ? 'Merci' : ''
+                }
+            },
+            btnState() {
+                if (this.btnState1 & this.btnState2 & this.btnState3 & this.btnState4 ) {
+                    return false;
+                }
+                return true;
+            }
+        },
         data() {
             return {
+                btnState1: false,
+                btnState2: false,
+                btnState3: false,
+                btnState4: false,
                 contactFirstname: '',
                 contactLastname: '',
                 contactEmail: '',
+                pMsg: '',
                 contactMessage: ''
             }
         },
@@ -72,10 +201,17 @@
                     mail: this.contactEmail,
                     message: this.contactMessage
                 });
+                this.pMsg.style.display = 'block'
+                this.contactLastname = '';
+                this.contactFirstname = '';
+                this.contactEmail = '';
+                this.contactMessage = '';
             }
         },
         mounted() {
             console.log("Component mounted.");
+            this.pMsg = document.getElementById("pMsg")
+            this.pMsg.style.display = 'none'
         }
     };
 
